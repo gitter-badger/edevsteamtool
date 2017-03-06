adb2a6632a5fdb2fb83334fcc08a7b8da41e9113
Alexandre Ferreira <alex.jorge.m@gmail.com>
2017-03-06 02:39:50 +0000

    read

---
+++ etc/steamcmd258550_mini.bat
@@ -0,0 +1,4 @@
+@echo off
+set run=%~dp0steamcmd258550
+CD %run%
+RustDedicated.exe -batchmode +server.hostname "EDev Rust Dedicated Server" +server.port 28015 +server.identity "edevrust_mini" +server.tickrate 30 +server.maxplayers 6 +server.worldsize 1000 +server.saveinterval 600 +rcon.port 28016 +rcon.ip 0.0.0.0 +rcon.password "edevrust123" -logfile "rustlog_mini"
